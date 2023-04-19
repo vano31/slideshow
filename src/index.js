87cd70b429ded590217fd82e9bd5f66cccac5a9a
vano31 <vanosike31@gmail.com>
2023-04-19 19:55:02 -0400

    Made background squares toggleable for visibility

--- src/index.js
+++ src/index.js
@@ -1,7 +1,68 @@
 import './style.css';
 import {mainpage} from './structure.js'
 
-let {page, menuMain, menuSub, imageHolder, conveyerBelt, timeDisplay, backgroundDisplay} = mainpage();
+let {page, menuMain, menuSub, imageHolder, conveyerBelt, timeDisplay, backgroundDisplay, dropDownImage, dropDownColor} = mainpage();
+
+
+let redColorSquare = document.createElement('div');
+let greenColorSquare = document.createElement('div');
+let blueColorSquare = document.createElement('div');
+let goldColorSquare = document.createElement('div');
+let silverColorSquare = document.createElement('div');
+
+redColorSquare.classList.add('backgroundSquare');
+greenColorSquare.classList.add('backgroundSquare');
+blueColorSquare.classList.add('backgroundSquare');
+goldColorSquare.classList.add('backgroundSquare');
+silverColorSquare.classList.add('backgroundSquare');
+
+redColorSquare.id = 'red';
+greenColorSquare.id = 'green';
+blueColorSquare.id = 'blue';
+goldColorSquare.id = 'gold';
+silverColorSquare.id = 'silver';
+
+redColorSquare.textContent = 'red';
+redColorSquare.setAttribute('style', 'visibility: hidden');
+menuSub.appendChild(redColorSquare);
+
+greenColorSquare.textContent = 'green';
+greenColorSquare.setAttribute('style', 'visibility: hidden')
+menuSub.appendChild(greenColorSquare);
+
+blueColorSquare.textContent = 'blue';
+blueColorSquare.setAttribute('style', 'visibility: hidden')
+menuSub.appendChild(blueColorSquare);
+
+goldColorSquare.textContent = 'gold';
+goldColorSquare.setAttribute('style', 'visibility: hidden');
+menuSub.appendChild(goldColorSquare);
+
+silverColorSquare.textContent = 'silver';
+silverColorSquare.setAttribute('style', 'visibility: hidden');
+menuSub.appendChild(silverColorSquare);
+
+let backgroundSquares = document.querySelectorAll('.backgroundSquare');
+console.log(backgroundSquares);
+
+//let backgroundSquares = document.querySelectorAll('.backgroundSquare');
+//let gook = document.querySelectorAll('#red');
+
+dropDownColor.addEventListener('click', () => {
+
+    backgroundSquares.forEach(square => {
+        if (square.style.visibility === 'hidden') {
+
+            square.style.visibility = 'visible';
+
+        }   else {
+            square.style.visibility = 'hidden';
+        }
+    })
+
+
+
+})
 
 
 
@@ -9,10 +70,3 @@ let {page, menuMain, menuSub, imageHolder, conveyerBelt, timeDisplay, background
 
 
 
-/*
-console.log(`Dooookie`);
-
-let title = document.createElement('div');
-title.textContent = 'Title goes herecd ';
-document.body.appendChild(title);
-*/
--- src/structure.js
+++ src/structure.js
@@ -1,16 +1,55 @@
 let mainpage = function() {
 
-
+///////////////////////////////////////////////
 let page = document.createElement('div');
 page.classList.add('page');
 page.classList.add('background');  
 document.body.appendChild(page);
 
+//////////////////////////////////////////////////////
 let menuMain = document.createElement('div');
 menuMain.classList.add('menu');
 menuMain.classList.add('main')
 menuMain.classList.add('background');
 page.appendChild(menuMain);
+
+let dropDownImage = document.createElement('button');
+dropDownImage.id = 'dropDownImage';
+dropDownImage.textContent = 'Choose Image';
+menuMain.appendChild(dropDownImage);
+
+let dropDownColor = document.createElement('button');
+dropDownColor.id = 'dropDownColor';
+dropDownColor.textContent = 'Choose Color';
+menuMain.appendChild(dropDownColor);
+
+/////////////////////////////////////////////////
+
+let colorbox = document.createElement('button');
+
+//Create colorboxx buttons with for loop instead of defining each one individually
+
+
+
+
+/////////////////////////////////////////////////
+
+dropDownColor.addEventListener('click', () => {
+
+    
+
+
+
+
+})
+
+dropDownImage.addEventListener('click', () => {
+
+
+
+})
+
+///////////////////////////////////////////////////
 
 let menuSub = document.createElement('div');
 menuSub.classList.add('menu');
@@ -18,15 +57,23 @@ menuSub.classList.add('sub')
 menuSub.classList.add('background');
 page.appendChild(menuSub);
 
+
+
+///////////////////////////////////////////////////////
+
 let imageHolder = document.createElement('div');
 imageHolder.classList.add('imageHolder');
 imageHolder.classList.add('background');
 page.appendChild(imageHolder);
 
+//////////////////////////////////////////////////////////
+
 let conveyerBelt = document.createElement('div');
 conveyerBelt.classList.add('conveyerBelt');
 conveyerBelt.classList.add('background');
 page.appendChild(conveyerBelt);
+
+//////////////////////////////////////////////////////////
 
 let bottomSection = document.createElement('div');
 bottomSection.classList.add('bottomSection');
@@ -43,9 +90,9 @@ backgroundDisplay.classList.add('backgroundDisplay');
 backgroundDisplay.classList.add('background');
 bottomSection.appendChild(backgroundDisplay);
 
+///////////////////////////////////////////////////////////////
 
-
-return {page, menuMain, menuSub, imageHolder, conveyerBelt, bottomSection, timeDisplay, backgroundDisplay}
+return {page, menuMain, menuSub, imageHolder, conveyerBelt, bottomSection, timeDisplay, backgroundDisplay, dropDownImage, dropDownColor}
 
 }
 
